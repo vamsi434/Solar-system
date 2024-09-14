d7bec78b2317623da43705003d8ed1c11f258d2a
vamsi434 <83161676+vamsi434@users.noreply.github.com>
2024-09-14 07:32:54 +0000

    change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(mongodb+srv://supercluster.d83jj.mongodb.net/superData, {
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
     user: 'superuser',
     pass: 'SuperPassword',
     useNewUrlParser: true,
